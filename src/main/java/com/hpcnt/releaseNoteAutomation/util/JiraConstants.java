d729d0ace8dd80383e58822696912e7a8ca07ddf
owen151128 <owen151128@gmail.com>
2018-04-26 14:48:32 +0900

    상수 추가
    BLANK -> 공백
    NULL -> 빈 문자열
    DRIVE_PATH -> 구글 드라이브 경로 고유 아이디 ( 폴더 고유 값 )
    
    구글 닥스에 작성에 필요한 상수들
    AOS_TITLE -> 구글닥스 AOS 제목
    IOS_TITLE -> 구글닥스 IOS 제목
    NUMBER -> NO
    ISSUE_TYPE -> 문자열 상수
    ISSUE_KEY -> 문자열 상수
    SUMMARY -> 문자열 상수
    STATUS -> 문자열 상수
    LABELS -> 문자열 상수
    DEFAULT_RANGE -> 기본 구글 닥스 작성 범위
    RANGE_PREFIX -> 동적으로 작성 범위를 조절하기 위해서 필요한 값
    SHEET_NAME -> 시트 이름 탬플릿

--- src/main/java/com/hpcnt/releaseNoteAutomation/util/JiraConstants.java
+++ src/main/java/com/hpcnt/releaseNoteAutomation/util/JiraConstants.java
@@ -9,7 +9,18 @@ package com.hpcnt.releaseNoteAutomation.util;
  */
 public class JiraConstants {
 
-	/*
+	/**
+	 * BLANK
+	 */
+	public static final String BLANK = " ";
+	public static final String NULL = "";
+
+	/**
+	 * Google drive path
+	 */
+	public static final String DRIVE_PATH = "1xld-si3Ci5W_3BydGxZI129KMVSxbjkW";
+
+	/**
 	 * JIRA base URL
 	 */
 	public static final String BASE_URL = "https://hyperconnect.atlassian.net/";
@@ -29,6 +40,8 @@ public class JiraConstants {
 	 */
 	public static final String USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/57.0.2987.133 Safari/537.36";
 	public static final String REST_USER_AGENT = "Mozilla";
+
+	public static final String MIME_TYPE = "application/vnd.google-apps.spreadsheet";
 
 	/**
 	 * JSON parsing constants
@@ -60,9 +73,9 @@ public class JiraConstants {
 	public static final String NAME = "name";
 	public static final String NAME_PATTERN = "ajs-remote-user-fullname";
 	public static final String NONE = "none";
-	
-	public static final String GET_ISSUE_SELECTOR="div > div > section > ul > li";
-	public static final String GET_VERSION_SELECTOR="head > meta[name=ajs-version-name]";
+
+	public static final String GET_ISSUE_SELECTOR = "div > div > section > ul > li";
+	public static final String GET_VERSION_SELECTOR = "head > meta[name=ajs-version-name]";
 
 	/**
 	 * ReleaseNote OS constants
@@ -71,6 +84,25 @@ public class JiraConstants {
 	public static final int AZIOS = 1;
 	public static final String AOS = "AZAND";
 	public static final String IOS = "AZIOS";
+
+	/**
+	 * Google spreadsheet constants
+	 */
+	public static final String AOS_TITLE = "Android";
+	public static final String IOS_TITLE = "Ios";
+	public static final String NUMBER = "NO";
+	public static final String ISSUE_TYPE = "Issue Type";
+	public static final String ISSUE_KEY = "IssueKey";
+	public static final String SUMMARY = "Summary";
+	public static final String STATUS = "Status";
+	public static final String LABELS = "Labels";
+	public static final String DEFAULT_RANGE = "B2:G2";
+	public static final String RANGE_PREFIX = "B3:G";
+
+	/**
+	 * Google sheet name
+	 */
+	public static final String SHEET_NAME = "_Release QA";
 
 	/**
 	 * REGEX constants
